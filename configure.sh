880f0d834c7000c6c52609eb63bda4a825b1325b
Nick Randell <nick@nickrandell.com>
2014-10-02 15:04:09 +0100

    Updated templates

--- configure.sh
+++ configure.sh
@@ -15,7 +15,7 @@ INDEX=0
 while [ $INDEX -lt $SERVER_COUNT ]
 do
 	ID=$(($INDEX+1))
-	BROKER_HOST="linode-london-infrastructure-$ID"
+	BROKER_HOST="linode-london-infrastructure-$ID.linode.nickrandell.me"
 	sed -e "s/#JVM_MEM#/$JVM_MEM/" -e "s/#BROKER_HOST#/$BROKER_HOST/" kafka.service.template > kafka@$ID.service
 	let INDEX=INDEX+1
 done
