161a6ae8a4048f6d54fafe0bf74b3cf9d3922329
Nick Randell <nick@nickrandell.com>
2014-10-02 15:03:09 +0100

    Updated templates

--- configure.sh
+++ configure.sh
@@ -6,25 +6,17 @@ then
   exit -1
 fi
 
-rm kafka*.service
+rm -f kafka*.service
 
 JVM_MEM=$1
 SERVER_COUNT=$2
-
-while read ip
-do
-	IPS+=("$ip:2181")
-done < <(fleetctl list-units  -no-legend | grep 'zookeeper...service' | grep  active | cut -f 2 | cut -d / -f 2)
-
-ALLIPS=$(IFS=","; echo "${IPS[*]}")
-echo $ALLIPS
 
 INDEX=0
 while [ $INDEX -lt $SERVER_COUNT ]
 do
 	ID=$(($INDEX+1))
-	IP=${IPS[$INDEX]}
-	sed -e "s/#ZOOKEEPERS#/$ALLIPS/" -e "s/#JVM_MEM#/$JVM_MEM/" kafka.service.template > kafka@$ID.service
+	BROKER_HOST="linode-london-infrastructure-$ID"
+	sed -e "s/#JVM_MEM#/$JVM_MEM/" -e "s/#BROKER_HOST#/$BROKER_HOST/" kafka.service.template > kafka@$ID.service
 	let INDEX=INDEX+1
 done
 
--- kafka-centos/start.sh
+++ kafka-centos/start.sh
@@ -18,8 +18,18 @@ then
 	exit 1
 fi
 
+if [ "x${BROKER_HOST}" = "x" ]
+then
+	echo "BROKER_HOST must be set"
+	exit 1
+fi
 
-sed -e "s/^broker.id=.*$/broker.id=$BROKER_ID/" -e "s/^zookeeper.connect=.*$/zookeeper.connect=$ZOOKEEPERS/" $KAFKA_HOME/config/server.properties.template > $KAFKA_HOME/config/server.properties
+
+sed \
+	-e "s/^broker.id=.*$/broker.id=$BROKER_ID/" \
+	-e "s/^zookeeper.connect=.*$/zookeeper.connect=$ZOOKEEPERS/" \
+	-e "s/^advertised.host.name=.*$/advertised.host.name=$BROKER_HOST/" \
+	$KAFKA_HOME/config/server.properties.template > $KAFKA_HOME/config/server.properties
 
 sed -s "s/export KAFKA_HEAP_OPTS=.*$/export KAFKA_HEAP_OPTS=\"-Xmx${JVM_MEM} -Xms${JVM_MEM}\"/" $KAFKA_HOME/bin/kafka-server-start.sh > $KAFKA_HOME/bin/kafka-server-start-modified.sh
 chmod +x $KAFKA_HOME/bin/kafka-server-start-modified.sh
